e9802d792456837e7dc852ba5428731c68ec31a0
vikairiska <verhozinavika2006@gmail.com>
2023-09-27 22:22:15 +0300

    Add homework_01

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,7 +4,14 @@
 """
 
 
-def power_numbers():
+def power_numbers(*numbers):
+    sq = []
+
+    for n in numbers:
+        power = n ** 2
+        sq.append(power)
+
+    return sq
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
@@ -18,15 +25,33 @@ ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def prime(num):
+    if num <= 1:
+        return False
+    elif num <= 3:
+        return True
+    elif num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i ** 2 <= num:
+        if num % i == 0 or num % (i+2) == 0:
+            return False
+        i += 6
+    return True
+def filter_numbers(*numbers, filter = 'even'):
+    if filter == 'even':
+        return [x for x in numbers if x%2 == 0]
+    elif filter == 'even':
+        return [x for x in numbers if x%2 == 1]
+    else:
+        return [x for x in numbers if prime(x)]
 
-def filter_numbers():
-    """
-    функция, которая на вход принимает список из целых чисел,
-    и возвращает только чётные/нечётные/простые числа
-    (выбор производится передачей дополнительного аргумента)
+numbers = [1,4,5,7,9,1,0,10,13,21]
 
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
-    """
+even_num = filter_numbers(numbers, 'even')
+odd_num = filter_numbers(numbers, 'odd')
+prime_num = filter_numbers(numbers, 'prime')
+
+print(even_num)
+print(odd_num)
+print(prime_num)
